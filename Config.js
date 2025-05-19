94e5afef0d1ee58854d09ba2ddf456510f33fa57
Dilsond <dilsond744@gmail.com>
2025-05-19 18:38:32 +0100

    06

--- Config.js
+++ Config.js
@@ -1,16 +1,9 @@
 export default class Config {
-<<<<<<< HEAD
   static API_URL = 'https://5561-154-71-159-172.ngrok-free.app/';
   static APP_NAME = 'MeuApp';
   static API_URL_WS = '5561-154-71-159-172.ngrok-free.app';
   static API_MEDIA_URL = 'https://5561-154-71-159-172.ngrok-free.app';
-=======
-  static API_URL = 'https://f72c-154-71-159-172.ngrok-free.app/';
-  static APP_NAME = 'MeuApp';
-  static API_URL_WS = 'f72c-154-71-159-172.ngrok-free.app';
-  static API_MEDIA_URL = 'https://f72c-154-71-159-172.ngrok-free.app';
->>>>>>> c10c5ecb9a050a532cb0303346a3b8c99f1de5bd
-
+  
   static getApiUrlMedia() {
     return this.API_MEDIA_URL;
   }
