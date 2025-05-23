2a30b98ebc1426ba8492e4aea0b289f39c7e2003
Dilsond <dilsond744@gmail.com>
2025-05-23 12:55:50 +0100

    07

--- src/app/dashboard/posto/page.tsx
+++ src/app/dashboard/posto/page.tsx
@@ -120,14 +120,6 @@ export default function Page(): React.JSX.Element {
       <Stack direction="row" spacing={3}>
         <Stack spacing={1} sx={{ flex: '1 1 auto' }}>
           <Typography variant="h4">Postos</Typography>
-          <Stack direction="row" spacing={1} sx={{ alignItems: 'center' }}>
-            <Button color="inherit" startIcon={<UploadIcon fontSize="var(--icon-fontSize-md)" />}>
-              Importar
-            </Button>
-            <Button color="inherit" startIcon={<DownloadIcon fontSize="var(--icon-fontSize-md)" />}>
-              Exportar
-            </Button>
-          </Stack>
         </Stack>
         <div>
           <Button startIcon={<PlusIcon fontSize="var(--icon-fontSize-md)" />} variant="contained">
